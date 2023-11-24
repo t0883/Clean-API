5eee6ee192fd3276d8f723f215488cea48d3214c
Tobias Andersson <tobias.andersson@student.nbi-handelsakademin.se>
2023-11-24 15:18:38 +0100

    Added a new model for cat to the project. Added controller and test aswell.

---
+++ Application/Queries/Cats/GetById/GetCatByIdQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Models;
+using Infrastructure.Database;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Queries.Cats.GetById
+{
+    public class GetCatByIdQueryHandler : IRequestHandler<GetCatByIdQuery, Cat>
+    {
+        private readonly MockDatabase _mockDatabase;
+
+        public GetCatByIdQueryHandler(MockDatabase mockDatabase)
+        {
+            _mockDatabase = mockDatabase;
+        }
+
+        public Task<Cat> Handle(GetCatByIdQuery request, CancellationToken cancellationToken)
+        {
+            Cat wantedCat = _mockDatabase.Cats.Where(cat => cat.Id == request.Id).FirstOrDefault()!;
+            return Task.FromResult(wantedCat);
+        }
+    }
+}
---
+++ Test/CatTests/QueryTest/GetCatByIdTests.cs
@@ -0,0 +1,57 @@
+﻿using Application.Queries.Cats.GetById;
+using Infrastructure.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.CatTests.QueryTest
+{
+    [TestFixture]
+    public class GetCatByIdTests
+    {
+        private GetCatByIdQueryHandler _handler;
+        private MockDatabase _mockDatabase;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Initialize the handler and mock database before each test
+            _mockDatabase = new MockDatabase();
+            _handler = new GetCatByIdQueryHandler(_mockDatabase);
+        }
+
+        [Test]
+        public async Task Handle_ValidId_ReturnCorrectCat()
+        {
+            // Arrange 
+            var catId = new Guid("12345678-1234-5678-1234-567812345601");
+
+            var query = new GetCatByIdQuery(catId);
+
+            // Act
+            var result = await _handler.Handle(query, CancellationToken.None);
+
+            // Assert 
+            Assert.NotNull(result);
+            Assert.That(result.Id, Is.EqualTo(catId));
+
+        }
+
+        [Test]
+        public async Task Handle_InvalidId_RetrunsNull()
+        {
+            // Arrange
+            var invalidCatId = Guid.NewGuid();
+
+            var query = new GetCatByIdQuery(invalidCatId);
+
+            // Act
+            var result = await _handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            Assert.IsNull(result);
+        }
+    }
+}
