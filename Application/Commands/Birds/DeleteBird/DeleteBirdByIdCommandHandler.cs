436baa32d2d8e89fce539c2dec681ae245604719
Tobias Andersson <tobias.andersson@student.nbi-handelsakademin.se>
2023-11-29 14:23:35 +0100

    Added a delete method for birds and test for that method

---
+++ Application/Commands/Birds/DeleteBird/DeleteBirdByIdCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Models;
+using Infrastructure.Database;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Commands.Birds.DeleteBird
+{
+    public class DeleteBirdByIdCommandHandler : IRequestHandler<DeleteBirdByIdCommand, Bird>
+    {
+        private readonly MockDatabase _mockDatabase;
+
+        public DeleteBirdByIdCommandHandler(MockDatabase mockDatabase)
+        {
+            _mockDatabase = mockDatabase;
+        }
+
+        public Task<Bird> Handle(DeleteBirdByIdCommand request, CancellationToken cancellationToken)
+        {
+            Bird birdToDelete = _mockDatabase.Birds.Where(bird => bird.Id == request.Id).FirstOrDefault()!;
+
+            _mockDatabase.Birds.Remove(birdToDelete);
+
+            return Task.FromResult(birdToDelete);
+        }
+    }
+}
