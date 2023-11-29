2549c2b18d434589f4abb360b84da445bfe39fec
Tobias Andersson <tobias.andersson@student.nbi-handelsakademin.se>
2023-11-29 14:08:02 +0100

    Added post method to api for birds. Added test for the post method.

---
+++ Test/BirdTests/CommandTest/AddBirdTests.cs
@@ -0,0 +1,46 @@
+﻿using Application.Commands.Birds.AddBird;
+using Application.Dtos;
+using Infrastructure.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.BirdTests.CommandTest
+{
+    [TestFixture]
+    public class AddBirdTests
+    {
+        private AddBirdCommandHandler _handler;
+        private MockDatabase _mockDatabase;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockDatabase = new MockDatabase();
+            _handler = new AddBirdCommandHandler(_mockDatabase);
+        }
+
+        [Test]
+        public async Task Handle_Add_Bird_To_MockDatabase()
+        {
+            //Arrange
+            var birdName = "Tage";
+
+            var dto = new BirdDto();
+
+            dto.Name = birdName;
+
+            dto.CanFly = false;
+
+            var command = new AddBirdCommand(dto);
+
+            //Act 
+            var result = await _handler.Handle(command, CancellationToken.None);
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
