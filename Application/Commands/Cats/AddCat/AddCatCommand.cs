71b2382dc2497f2ed488a602976d2dbb49f57b36
Tobias Andersson <tobias.andersson@student.nbi-handelsakademin.se>
2023-12-20 14:15:39 +0100

    Changed the process for adding cats to database. A userId is now required for adding cats to database.

--- Application/Commands/Cats/AddCat/AddCatCommand.cs
+++ Application/Commands/Cats/AddCat/AddCatCommand.cs
@@ -1,21 +1,18 @@
 ﻿using Application.Dtos;
 using Domain.Models;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.Commands.Cats.AddCat
 {
     public class AddCatCommand : IRequest<Cat>
     {
-        public AddCatCommand(CatDto newCat)
+        public AddCatCommand(CatDto newCat, Guid userId)
         {
             NewCat = newCat;
+            UserId = userId;
         }
 
         public CatDto NewCat { get; }
+        public Guid UserId { get; set; }
     }
 }
